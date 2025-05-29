c7fdb6ad0e85e465333a91c3f8ff9a11f5ab0926
Justas <ju.vaskelevicius@gmail.com>
2025-05-29 23:02:27 +0300

    getMyTeams.ts backend, not checked but only frontend left

--- src/lib/db/getMyTeams.ts
+++ src/lib/db/getMyTeams.ts
@@ -1,14 +1,47 @@
+"use server"
+
 import { getUserSession } from "./helpers";
 
-type TeamCardType = {
-    ageGroup: string;
-    joinCode?: string;
-    playerCount: number;
-    captainName: string;
-    name: string;
-    role: string;
+const db = require("@/src/lib/db/db");
+
+
+type TeamCardType =
+  | { success: true; teams: TeamInfo[] }
+  | { success: false };
+
+type TeamInfo = {
+  ageGroup: string;
+  joinCode?: string;
+  //playerCount: number;
+  //captainName: string;
+  name: string;
+  role: string;
 }
 
-export default function getMyTeams() {
-    const session = getUserSession()
+export default async function getMyTeams(): Promise<TeamCardType> {
+    const session = await getUserSession()
+
+    const sql = `SELECT teams.* FROM teams 
+    JOIN team_member ON team_member.team_id = teams.team_id 
+    WHERE team_member.user_id = ?`
+
+    try {
+        const response = await db.query(sql, [session.userId])
+
+        if (!response || response.length === 0) {
+            return {success: false}
+        }
+
+        const teams: TeamInfo[] = response.map((team: any) => ({
+            name: team.name,
+            ageGroup: team.age_group,
+            joinCode: team?.join_code,
+            role: team.role
+        }))
+
+        return {success: true, teams}
+    } catch(e) {
+        console.log("Error fetching teams", e)
+        return {success: false}
+    }
 }
